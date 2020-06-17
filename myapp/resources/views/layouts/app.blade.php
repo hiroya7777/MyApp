52b2c6b0b0ef2538de295842cccf1aafc9bfb299
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-06-17 13:31:42 +0900

    トップ画面からプロフィール画面へ遷移する #16

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -54,6 +54,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/profile') }}">
+                                        プロフィールをみる
+                                    </a>
+                                    
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
